98229139a05f5a7c6f7d1ac8231b19bf6c17d43c
marleigh <camarleigh@gmail.com>
2015-06-17 19:14:59 -0700

    added id row to the table. repopulated table & description fields.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,16 +3,16 @@ def random_time
 end
 
 tasks = [
-  { task_name: "The First Task", task_description: "", task_completed: random_time },
-  { task_name: "Go to Brunch", task_description: "" },
-  { task_name: "Go to Lunch", task_description: "", task_completed: random_time },
-  { task_name: "Go to Second Lunch", task_description: "" },
-  { task_name: "Play Video Games", task_description: "", task_completed: random_time },
-  { task_name: "High Five Somebody You Don't Know", task_description: "", task_completed: random_time },
-  { task_name: "Plant Flowers", task_description: "", task_completed: random_time },
-  { task_name: "Call Mom", task_description: "" },
-  { task_name: "She worries, you know.", task_description: "" },
-  { task_name: "Nap.", task_description: "", task_completed: random_time }
+  { task_name: "Buy lots of gin", task_description: "Every pool party needs gin.", task_completed: random_time },
+  { task_name: "Secure a pool", task_description: "Every pool party needs gin." },
+  { task_name: "Make guacamole", task_description: "Glad I have an avacado tree.", task_completed: random_time },
+  { task_name: "Start a Spotify List", task_description: "Need tunes for the party." },
+  { task_name: "Share Spotify Pool Party List Access", task_description: "The more tunes the bettah.", task_completed: random_time },
+  { task_name: "High Five Somebody You Don't Know", task_description: "Include yourself.", task_completed: random_time },
+  { task_name: "Invite ALLLL the Adies", task_description: "Cohort 0 + 1 + 2 = FUN!", task_completed: random_time },
+  { task_name: "Call Mom", task_description: "Invite her too!" },
+  { task_name: "Pre-order Pizza", task_description: "Ask Adies for preferences." },
+  { task_name: "Nap.", task_description: "Planning a pool party is freakin' hard!", task_completed: random_time }
 ]
 
 tasks.each do |task|
